803959938de06660f011f7c75c5c04505a4b4bc6
whiteflags26 <whiteflags26@gmail.com>
2025-03-13 23:05:20 +0600

    chore: add initial app and server entry files

---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+// import express from 'express';
+// import { register, login, getProfile, updateProfile, getAllUsers } from './controller';
+// import { protect, authorize } from '../../middlewares/authMiddleware';
+
+// const router = express.Router();
+
+// router.post('/register', register);
+// router.post('/login', login);
+// router.get('/profile', protect, getProfile);
+// router.put('/profile', protect, updateProfile);
+// router.get('/', protect, authorize('ADMIN', 'TRANSPORT_OFFICER'), getAllUsers);
+
+// export default router;
